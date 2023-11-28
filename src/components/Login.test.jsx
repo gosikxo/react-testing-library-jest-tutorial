4878c48f44877918b8353425f312d6fa56a83825
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-11-28 16:26:10 +0100

    Finish project

--- src/components/Login.test.jsx
+++ src/components/Login.test.jsx
@@ -125,3 +125,20 @@ test("loading should not be rendered after fetching", async () => {
     expect(buttonEl).not.toHaveTextContent(/please wait/i)
   })
 })
+
+test("user should be rendered after fetching", async () => {
+  render(<Login />)
+  const buttonEl = screen.getByRole("button")
+  const usernameInputEl = screen.getByPlaceholderText(/username/i)
+  const passwordInputEl = screen.getByPlaceholderText(/password/i)
+
+  const testValue = "test"
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } })
+  fireEvent.change(passwordInputEl, { target: { value: testValue } })
+  fireEvent.click(buttonEl)
+
+  const userItem = await screen.findByText("Leanne")
+
+  expect(userItem).toBeInTheDocument()
+})
